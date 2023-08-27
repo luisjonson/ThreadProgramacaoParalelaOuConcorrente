30acc1265ca2d9a90bc8514b4b22fc2a621f3b38
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-08-26 23:49:12 -0300

    AULA - Controlando a Thead 1 com Start e Stop

--- jdevThread/src/cursojava/thread/TelaTimeThread.java
+++ jdevThread/src/cursojava/thread/TelaTimeThread.java
@@ -5,6 +5,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -26,6 +30,23 @@ public class TelaTimeThread extends JDialog{
 	
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
+	
+	private Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			while (true) {/*Fica sempre Rodando*/
+				mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+	
+	private Thread thread1Time;
 	
 	
 	public TelaTimeThread() {
@@ -72,7 +93,22 @@ public class TelaTimeThread extends JDialog{
 		jPanel.add(jButton2, gridBagConstraints);
 		
 		
+		jButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+			}
+		});
 		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				thread1Time.stop();
+			}
+		});
 		
 		
 		add(jPanel,BorderLayout.WEST);
