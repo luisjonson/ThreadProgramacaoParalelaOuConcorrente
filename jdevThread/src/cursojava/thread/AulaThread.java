7de8b27affa6b70dc22c93756c77273da5eb6eef
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-08-26 21:55:11 -0300

    AULA - Processamento concorrente entre duas Thread

--- jdevThread/src/cursojava/thread/AulaThread.java
+++ jdevThread/src/cursojava/thread/AulaThread.java
@@ -14,7 +14,27 @@ public class AulaThread {
 						e.printStackTrace();
 					}
 					/* Quero exercultar esse envio com um tempo de parada,ou com tempodeterminado */
-					System.out.println("Exercultando alguma rotina" + pos);
+					System.out.println("Exercultando alguma rotina envio de email" + pos);
+				}
+
+			};
+
+		}.start();// liga a thread que fica processando paralelamente por tr´s
+		/* Codigo da rotina que eu quero exerculta em paralelo. */
+//		System.out.println("Chegou ao fim do codigo");
+//		// fluxo de venda
+//		JOptionPane.showMessageDialog(null, "Sistema continua para o usuário");
+		
+		new Thread() {
+			public void run() {
+				for (int pos = 0; pos < 10; pos++) {
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					/* Quero exercultar esse envio com um tempo de parada,ou com tempodeterminado */
+					System.out.println("Exercultando alguma rotina envio de nota fiscall" + pos);
 				}
 
 			};
@@ -22,8 +42,6 @@ public class AulaThread {
 		}.start();// liga a thread que fica processando paralelamente por tr´s
 		/* Codigo da rotina que eu quero exerculta em paralelo. */
 		System.out.println("Chegou ao fim do codigo");
-		// fluxo de venda
-		JOptionPane.showMessageDialog(null, "Sistema continua para o usuário");
 	}
 
 }
